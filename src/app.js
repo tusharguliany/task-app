08a8b846ccd6d5746cb76a5bc36084d3bc508083
Tushar Arora <tusharrguliany@gmail.com>
2020-07-16 16:19:44 +0530

    wrote test suites for task and user

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = process.env.PORT
+
+app.use(express.json())
+app.use(cors())
+app.use(userRouter)
+app.use(taskRouter)
+
+module.exports = app
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-const express = require('express')
-const cors = require('cors')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
 
-app.use(express.json())
-app.use(cors())
-app.use(userRouter)
-app.use(taskRouter)
+/* 
+    Steps to get this Project Working :
+    1) Set Up Environment Variables Directory. i.e. "config" in the root directory
+    2) Create Two Files in it with file names -> "dev.env" -- is for Dev Environment i.e. npm run dev
+                                                 "test.env" -- is for Test Enviroment i.e. npm test
+    3) The File needs to have the following variables
+        PORT=<< your port number like 3000 >>
+        SENDGRID_API_KEY=<< your SENDGRID Api Key >>
+        JWT_SECRET=<< your secret key for json web token, it can be anything like 'hey i am doing it' >>
+        MONGO_DB_URL=<< your connection string to mongodb server and database name e.g. mongodb://127.0.0.1:27017/<yourDBName> >>
+    4) Set the Environment Variables in your hosting, I use heroku and the way to set is :
+        heroku config:set key=value
+        -- You can check if it worked or not by 'heroku config'
+    5) npm run dev
+*/
 
 app.listen(port, () => {
     console.log(`Server is up on Port ${port}`)
